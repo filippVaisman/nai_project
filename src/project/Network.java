7b8ee3d4ba8ae841b8b67b639408a005556b2c5d
filippVaisman <vaisman1626@gmail.com>
2018-06-13 15:41:42 +0200

    function refactor

--- src/project/Network.java
+++ src/project/Network.java
@@ -1,8 +1,11 @@
 package project;
 
+import sun.nio.ch.Net;
+
 public class Network {
     protected Neuron[] hiddenLayer;
     protected Neuron[] outerLayer;
+    NetworkCalculator calculator;
     private double[][] inputs = {
             {0, 1, 6, 15, 12, 1, 0, 0, 0, 7, 16, 6, 6, 10, 0, 0, 0, 8, 16, 2, 0, 11, 2, 0, 0, 5, 16, 3, 0, 5, 7, 0, 0, 7, 13, 3, 0, 8, 7, 0, 0, 4, 12, 0, 1, 13, 5, 0, 0, 0, 14, 9, 15, 9, 0, 0, 0, 0, 6, 14, 7, 1, 0, 0}
     };
@@ -14,6 +17,7 @@ public class Network {
         this.hiddenLayer = new Neuron[hiddenLayerCount];
         this.outerLayer = new Neuron[outerLayerCount];
         this.lambda = lambda;
+        calculator = new NetworkCalculator(this);
         initNetwork();
     }
 
@@ -29,16 +33,14 @@ public class Network {
             outerLayer[i] = new Neuron(hiddenLayer.length, lambda);
         }
     }
+
+
     public double[] getOutput(double[] input){
-        double[] hiddenLayerOutput = new double[hiddenLayer.length];
-        double[] outerLayerOutput = new double[outerLayer.length];
-        for(int i = 0; i < hiddenLayer.length; i++){
-            hiddenLayerOutput[i] = hiddenLayer[i].getOutput(input);
-        }
-        for(int i = 0; i < outerLayer.length; i++){
-             outerLayerOutput[i] = outerLayer[i].getOutput(hiddenLayerOutput);
-        }
-        return outerLayerOutput;
+        return calculator.getOutput();
+    }
+
+    public double[][] getInputs() {
+        return inputs;
     }
 }
 
--- src/project/NetworkCalculator.java
+++ src/project/NetworkCalculator.java
@@ -5,17 +5,25 @@ public class NetworkCalculator {
     private Network network;
     private double outputLayerError;
     private double hiddenLayerError;
+    private double[] output;
 
     public NetworkCalculator(Network network) {
         this.network = network;
         outputLayerError = 0.0d;
         hiddenLayerError = 0.0d;
+
     }
 
-    private double[] calculateErrorOnHiddenLayer (){
-
-
-        return null;
+    public double[] getOutput (double[] input){
+        double[] hiddenLayerOutput = new double[network.hiddenLayer.length];
+        double[] outerLayerOutput = new double[network.outerLayer.length];
+        for(int i = 0; i < network.hiddenLayer.length; i++){
+            hiddenLayerOutput[i] = network.hiddenLayer[i].getOutput(input);
+        }
+        for(int i = 0; i < network.outerLayer.length; i++){
+            outerLayerOutput[i] = network.outerLayer[i].getOutput(hiddenLayerOutput);
+        }
+        return outerLayerOutput;
     }
 
 
